e25b8f407bcefe27be955e35eaf1b502ce527c2a
Lewis Harvey <lehar@netcompany.com>
2022-12-14 22:55:54 +0000

    feat: remove some boiler plate stuff

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -39,14 +39,6 @@ const Main = (props: IMainProps) => (
               </Link>
             </li>
             <li className="mr-6">
-              <a
-                className="border-none text-gray-700 hover:text-gray-900"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
-              >
-                GitHub
-              </a>
-            </li>
-            <li className="mr-6">
               <Link
                 href="/blog/"
                 className="border-none text-gray-700 hover:text-gray-900"
