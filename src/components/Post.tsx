0cd745d8d7aab9acacd2e91f74475c35d58c60b7
Lewis Harvey <lehar@netcompany.com>
2022-12-15 19:31:17 +0000

    feat: built component for post

---
+++ src/components/Post.tsx
@@ -0,0 +1,47 @@
+type IPostProps = {
+  fullName: string;
+  postText: string;
+  likes: Array<IPersonRef>;
+  profilePicLink: string;
+};
+
+type IPersonRef = {
+  fullName: string;
+  href: string;
+};
+
+const Post = (props: IPostProps) => {
+  // lehar, this needs to be changed to the current users name
+  const currentUserName = 'Lewis Harvey';
+  return (
+    <div className="mb-4 rounded-lg border-2 border-gray-400 p-5">
+      <div className="flex items-center">
+        <img
+          className="w-10 rounded-full sm:w-14"
+          alt="profile pic"
+          src={props.profilePicLink}
+        ></img>
+        <span className="ml-5 text-lg sm:text-2xl">{props.fullName}</span>
+      </div>
+      <p className="text-xl">{props.postText}</p>
+      <div className="flex items-center">
+        <img alt="profile pic" className="mr-2 w-6" src="/icon-heart.svg"></img>
+        {props.likes.map((like, index) => {
+          return (
+            <>
+              <a key={index} href={like.href}>
+                <span className="text-purple-700">
+                  {like.fullName === currentUserName && 'You'}
+                  {like.fullName !== 'Lewis Harvey' && like.fullName}
+                </span>
+              </a>
+              {index !== props.likes.length - 1 && ', '}
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export { Post };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Post } from '@/components/Post';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -11,8 +12,16 @@ const Index = () => {
         />
       }
     >
-      <h1 className="text-2xl">All Recent Posts</h1>
-      <p>All Recent Posts</p>
+      <h1 className="text-3xl">All Recent Posts</h1>
+      <Post
+        fullName="Lewis Harvey"
+        postText="this is the post text"
+        profilePicLink="https://placekitten.com/60/60"
+        likes={[
+          { fullName: 'Lewis Harvey', href: 'google.com' },
+          { fullName: 'John Smith', href: 'google.com' },
+        ]}
+      ></Post>
     </Main>
   );
 };
